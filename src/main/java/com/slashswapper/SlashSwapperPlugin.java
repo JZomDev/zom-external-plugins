92db52fb597b3f1bd6a7b0d47ed21423153ccc08
redrumze <jzomerlei@gmail.com>
2023-01-14 21:49:28 -0500

    Auto format & fix comments

--- src/main/java/com/slashswapper/SlashSwapperPlugin.java
+++ src/main/java/com/slashswapper/SlashSwapperPlugin.java
@@ -1,18 +1,12 @@
 package com.slashswapper;
 
+import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.ScriptID;
-import net.runelite.api.VarClientStr;
-import net.runelite.api.clan.ClanID;
 import net.runelite.api.events.ScriptCallbackEvent;
-import net.runelite.api.events.ScriptPreFired;
-import net.runelite.api.vars.AccountType;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-
-import javax.inject.Inject;
 
 @Slf4j
 @PluginDescriptor(
@@ -34,10 +28,17 @@ public class SlashSwapperPlugin extends Plugin
 		final int[] intStack = client.getIntStack();
 		int intStackCount = client.getIntStackSize();
 
-		if (intStack[intStackCount-1] == 1 && intStack[intStackCount-2] == 9) { // message is going to friends chat
-			intStack[intStackCount-2] = 41;
-		}else if (intStack[intStackCount-1] == 2 && intStack[intStackCount-2] == 41) { // message is going to friends chat
-			intStack[intStackCount-2] = 9;
+		// intStack[intStackCount -1] is the check to see if a user typed / or // from non fc/cc selected channels
+		// intStack[intStackCount - 2] is the ID to see what channel the message is routed into
+		if (intStack[intStackCount - 1] == 1 && intStack[intStackCount - 2] == 9)
+		{
+			// message is going to friends chat because user typed /
+			intStack[intStackCount - 2] = 41; // swap it to clan chat
+		}
+		else if (intStack[intStackCount - 1] == 2 && intStack[intStackCount - 2] == 41)
+		{
+			// message is going to clan chat because user typed //
+			intStack[intStackCount - 2] = 9; // swap it to friends chat
 		}
 	}
 }
